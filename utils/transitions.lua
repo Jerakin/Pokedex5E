9c27c9194a81251ac94d619554ef4b370e4b72d6
Jerakin <the_rensvind@hotmail.com>
2019-02-05 20:39:36 +0100

    clearing stack when navigating

--- utils/transitions.lua
+++ utils/transitions.lua
@@ -6,8 +6,6 @@ function M.default(root_node)
 	local transition = transitions.create(root_node)
 	.show_in(transitions.slide_in_left, gui.EASING_OUTQUAD, 0.6, 0)
 	.show_out(transitions.slide_out_right, gui.EASING_INQUAD, 0.6, 0)
-	.back_in(transitions.slide_in_left, gui.EASING_OUTQUAD, 0.6, 0)
-	.back_out(transitions.slide_out_right, gui.EASING_INQUAD, 0.6, 0)
 	return transition
 end
 
