fcd3afb3b5a0462a54b5b7a34f5399d639f9a6df
Jerakin <the_rensvind@hotmail.com>
2019-02-11 19:12:09 +0100

    move color and icon

---
+++ utils/type_data.lua
@@ -0,0 +1,38 @@
+local M = {
+	Normal		= {icon = "element_normal",		color = vmath.vector3(128/255, 128/255, 128/255, 1)},
+	
+	Fire		= {icon = "element_fire",		color = vmath.vector3(240/255, 128/255, 48/255, 1)},
+	
+	Fighting	= {icon = "element_fighting",	color = vmath.vector3(192/255, 48/255, 40/255, 1)},
+	
+	Water		= {icon = "element_water",		color = vmath.vector3(104/255, 144/255, 244/255, 1)},
+	
+	Flying		= {icon = "element_flying",		color = vmath.vector3(168/255, 144/255, 240/255, 1)},
+	
+	Grass		= {icon = "element_grass",		color = vmath.vector3(120/255, 200/255, 80/255, 1)},
+	
+	Poison		= {icon = "element_poison",		color = vmath.vector3(160/255, 64/255, 160/255, 1)},
+	
+	Electric	= {icon = "element_electric",	color = vmath.vector3(248/255, 208/255, 48/255, 1)},
+	
+	Ground		= {icon = "element_ground",		color = vmath.vector3(224/255, 196/255, 104/255, 1)},
+	
+	Psychic		= {icon = "element_pshycic",	color = vmath.vector3(248/255, 88/255, 136/255, 1)},
+	
+	Rock		= {icon = "element_rock",		color = vmath.vector3(186/255, 160/255, 56/255, 1)},
+	
+	Ice			= {icon = "element_ice",		color = vmath.vector3(152/255, 216/255, 216/255, 1)},
+	
+	Bug			= {icon = "element_bug",		color = vmath.vector3(168/255, 184/255, 32/255, 1)},
+	
+	Dragon		= {icon = "element_dragon",		color = vmath.vector3(112/255, 56/255, 248/255, 1)},
+	
+	Ghost		= {icon = "element_ghost",		color = vmath.vector3(112/255, 88/255, 152/255, 1)},
+	
+	Dark		= {icon = "element_dark",		color = vmath.vector3(112/255, 88/255, 72/255, 1)},
+	
+	Steel		= {icon = "element_steel",		color = vmath.vector3(184/255, 184/255, 208/255, 1)},
+	
+	Fairy		= {icon = "element_fairy",		color = vmath.vector3(138/255, 153/255, 172/255, 1)}
+}
+return M
