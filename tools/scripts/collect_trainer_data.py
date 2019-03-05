3fda09aa562f83c4e2f459f795412444d8a7e8be
Jerakin <the_rensvind@hotmail.com>
2019-03-06 00:11:33 +0100

    Exluded pokemon stadium and extra games from trainer class pokemons

--- tools/scripts/collect_trainer_data.py
+++ tools/scripts/collect_trainer_data.py
@@ -3,29 +3,33 @@ import json
 import re
 import requests
 
-input_path = Path(r"D:\Repo\Pokedex\assets\datafiles\trainer_class_list.json")
-output_path = Path(r"D:\Repo\Pokedex\assets\datafiles\trainer_class.json")
+input_path = Path(r"D:\Repo\Pokedex\assets\datafiles\trainer_classes_list.json")
+output_path = Path(r"trainer_classes.json")
 
 dirty_reg = re.compile('title="(\d{3})"><img alt="\d{3}"')
 
 new_json = {}
+
+
+def clean_content(content):
+    pre_removed = content.split('id="Trainer_list">Trainer list', 1)[-1]
+    post_removed = pre_removed.split('''mon Stadium</span></h3>''')
+    return post_removed[-1]
+
 
 def main():
     with open(input_path, "r+") as f:
         data = json.load(f)
         for trainer in data["Classes"]:
             raw_url = "https://m.bulbapedia.bulbagarden.net/wiki/{}_(Trainer_class)".format(trainer.replace(" ", "_"))
+
             new_json[trainer] = []
-            try:
-                r = requests.get(raw_url)
-                for m in re.findall(dirty_reg, str(r.content)):
-                    new_json[trainer].append(m)
-            except:
-                print(trainer)
-                pass
+            r = requests.get(raw_url)
+            for m in re.findall(dirty_reg, clean_content(str(r.content))):
+                new_json[trainer].append(m)
+            print(trainer)
 
         with open(output_path, "w") as fp:
             json.dump(new_json, fp)
-
 
 main()
--- tools/scripts/convert_trainer_data.py
+++ tools/scripts/convert_trainer_data.py
@@ -2,12 +2,11 @@ from pathlib import Path
 import json
 
 
-input_path = Path("trainer_class.json")
-output_path = Path(r"D:\Repo\Pokedex\assets\datafiles\trainer_class.json")
+input_path = Path("trainer_classes.json")
+output_path = Path(r"D:\Repo\Pokedex\assets\datafiles\trainer_classes.json")
 pokemon_index = Path(r"D:\Repo\Pokedex\assets\datafiles\pokemon_order.json")
 
 index = {}
-
 
 with open(pokemon_index, "r") as f:
     data = json.load(f)
@@ -23,6 +22,7 @@ with open(input_path, "r") as f:
             pokemon = int(pokemon)
             if pokemon in index and index[pokemon] not in pokemon_list:
                 pokemon_list.append(index[pokemon])
+        pokemon_list.sort()
         new_json[trainer] = pokemon_list
     with open(output_path, "w") as fp:
-        json.dump(new_json, fp, ensure_ascii=False)
+        json.dump(new_json, fp, ensure_ascii=False, indent="  ")
