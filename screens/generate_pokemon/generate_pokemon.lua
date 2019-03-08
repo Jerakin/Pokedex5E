37a954548debc7667d4d05c21a766e11293581c6
Jerakin <the_rensvind@hotmail.com>
2019-03-09 00:10:42 +0100

    adding buttons

--- screens/generate_pokemon/generate_pokemon.lua
+++ screens/generate_pokemon/generate_pokemon.lua
@@ -1,4 +1,27 @@
--- Put functions in this file to use them in several other scripts.
--- To get access to the functions, you need to put:
--- require "my_directory.my_file"
--- in any script using the functions.
+local _pokemon = require "pokedex.pokemon"
+local storage = require "pokedex.storage"
+local pokedex = require "pokedex.pokedex"
+local nature = require "pokedex.natures"
+local movedex = require "pokedex.moves"
+
+local M = {}
+
+function M.add_pokemon(species, level)
+	level = level or 5
+	local starting_moves = pokedex.get_starting_moves(species)
+	local pokemon = _pokemon.new({species=species})
+	local moves = {}
+	for i=1, 4 do
+		if starting_moves[i] then
+			local pp = movedex.get_move_pp(starting_moves[i])
+			moves[starting_moves[i]] = {pp=pp, index=i}
+		end
+	end
+	pokemon.level.caught = level
+	pokemon.level.current = level
+	pokemon.moves = moves
+	pokemon.nature = nature.list[math.random(#nature.list)]
+	storage.add(pokemon)
+end
+
+return M
