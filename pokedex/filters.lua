fca8dd44a7cb8d3bc4e447f6523732fce258e194
Jerakin <the_rensvind@hotmail.com>
2019-03-10 22:41:47 +0100

    changing values and stuff

--- pokedex/filters.lua
+++ pokedex/filters.lua
@@ -9,11 +9,12 @@ local trainer_classes_list
 local habitats
 local initialized
 local sr = {}
+local minimum_level = {}
 
+local _pokedex
 
 local function species_rating()
-	local pokedex = file.load_json_from_resource("/assets/datafiles/pokemon.json")
-	for pokemon, data in pairs(pokedex) do
+	for pokemon, data in pairs(_pokedex) do
 		local cr = tostring(data.CR)
 		if sr[cr] then 
 			table.insert(sr[cr], pokemon)
@@ -23,17 +24,30 @@ local function species_rating()
 	end
 end
 
+local function minimum_found_level()
+	for pokemon, data in pairs(_pokedex) do
+		local min_lvl = tostring(data["MIN LVL FD"])
+		if minimum_level[min_lvl] then 
+			table.insert(minimum_level[min_lvl], pokemon)
+		else 
+			minimum_level[min_lvl] = {pokemon}
+		end
+	end
+end
 
 function M.init()
 	if not initialized then
+		_pokedex = file.load_json_from_resource("/assets/datafiles/pokemon.json")
 		trainer_classes = file.load_json_from_resource("/assets/datafiles/trainer_classes.json")
 		trainer_classes_list = file.load_json_from_resource("/assets/datafiles/trainer_classes_list.json")
 		habitats = file.load_json_from_resource("/assets/datafiles/habitat.json")
+		
 		local ordered = file.load_json_from_resource("/assets/datafiles/pokemon_order.json")
 		trainer_classes_list.Classes[#trainer_classes_list.Classes + 1] = "No Trainer"
 		habitats.All = ordered.number
 		trainer_classes["No Trainer"] = ordered.number
 		species_rating()
+		minimum_found_level()
 		initialized = true
 	end
 end
@@ -67,8 +81,27 @@ local function SR_list(min, max)
 	return n
 end
 
-function M.get_list(trainer_class, habitat, sr_min, sr_max)
-	return filter(filter(trainer_classes[trainer_class], habitats[habitat]), SR_list(sr_min, sr_max))
+local number_map = {["1/8"]=0.125, ["1/4"]=0.25, ["1/2"]=0.5, ["1"]=1, ["2"]=2, ["3"]=3, ["4"]=4, ["5"]=5, ["6"]=6, ["7"]=7,["8"]=8,
+["9"]=9, ["10"]=10, ["11"]=11, ["12"]=12, ["13"]=13, ["14"]=14, ["15"]=15}
+
+local function minimum_level_list(lvl)
+	local n = {}
+	for m_lvl, list in pairs(minimum_level) do
+		m_lvl = tonumber(m_lvl)
+		if m_lvl >= lvl then
+			for _, l in pairs(list) do
+				table.insert(n, l)
+			end
+		end
+	end
+	return n
+end
+
+function M.get_list(trainer_class, habitat, sr_min, sr_max, min_level)
+	local class_habitat = filter(trainer_classes[trainer_class], habitats[habitat]) 
+	local class_habitat_sr = filter(class_habitat, SR_list(sr_min, sr_max))
+	local class_habitat_sr_lvl = filter(minimum_level_list(min_level), class_habitat_sr, true)
+	return class_habitat_sr_lvl
 end
 
 function M.habitat_list()
