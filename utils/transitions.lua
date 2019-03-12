ab4d242c06bfd674a8b31ca540d12892d0a937bc
Jerakin <the_rensvind@hotmail.com>
2019-03-13 00:13:21 +0100

    Added notification system

--- utils/transitions.lua
+++ utils/transitions.lua
@@ -6,12 +6,16 @@ function M.default(root_node)
 	local transition = transitions.create(root_node)
 	.show_in(transitions.slide_in_left, gui.EASING_OUTQUAD, 0.6, 0)
 	.show_out(transitions.slide_out_right, gui.EASING_INQUAD, 0.6, 0)
+	.back_out(transitions.slide_out_right, gui.EASING_INQUAD, 0.6, 0)
 	return transition
 end
 
-
-function M.on_message(transition, message_id, message, sender)
-	transition.handle(message_id, message, sender)
+function M.notification(root_node)
+	local transition = transitions.create(root_node)
+	.show_in(transitions.slide_in_top, gui.EASING_OUTQUAD, 0.6, 0)
+	.show_out(transitions.slide_out_top, gui.EASING_INQUAD, 0.6, 0)
+	.back_out(transitions.slide_out_top, gui.EASING_INQUAD, 0.6, 0)
+	return transition
 end
 
 return M
