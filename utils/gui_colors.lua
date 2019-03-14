2318fb9789d67f1a4c895bfbb14fa0d0c1f580a6
teyisbae <baebaebae>
2019-03-14 22:02:37 +0100

    misc small tweaks

--- utils/gui_colors.lua
+++ utils/gui_colors.lua
@@ -1,9 +1,9 @@
 local M = {}
 
 M.INFO_PLATES = vmath.vector4(253/255, 241/255, 220/255, 1)
-M.BACKGROUND = vmath.vector4(236/255, 212/255, 171/255, 1)
-M.HERO_TEXT = vmath.vector4(121/255, 30/255, 11/255, 1)
-M.HERO_TEXT_FADED = vmath.vector4(121/255, 30/255, 11/255, 0.5)
+M.BACKGROUND = vmath.vector4(240/255, 240/255, 240/255, 1)
+M.HERO_TEXT = vmath.vector4(62/255, 62/255, 62/255, 1)
+M.HERO_TEXT_FADED = vmath.vector4(112/255, 112/255, 112/255, 0.5)
 M.BUTTON_TEXT = vmath.vector4(38/255, 36/255, 32/255, 1)
 M.BUTTON_TEXT_PRESSED = vmath.vector4(38/255, 36/255, 32/255, 1)
 M.INACTIVE = vmath.vector4(0.3, 0.3, 0.3, 1)
