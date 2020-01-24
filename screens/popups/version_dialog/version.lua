5c655c8cde7e600a56d81d475880dda0d614c1f6
Jerakin <The_rensvind@hotmail.com>
2020-01-24 16:17:44 +0100

    Added version dialog

---
+++ screens/popups/version_dialog/version.lua
@@ -0,0 +1,51 @@
+local flow = require "utils.flow"
+local M = {}
+
+local version_url = "https://raw.githubusercontent.com/Jerakin/Pokedex5E/version_dialog/assets/datafiles/releases.json"
+
+M.BUSY = false
+
+M.releases = nil
+
+
+
+function M.get_latest()
+	http.request(version_url, "GET", function(self, id, res)
+		print(res.status)
+		if res.status == 200 or res.status == 304 then
+			M.releases = json.decode(res.response)
+		else
+			M.releases = nil
+			gameanalytics.addErrorEvent {
+				severity = "Warning",
+				message = "Version:LoadIndex:HTTP:" .. res.status 
+			}
+			log.info("Version:BAD STATUS:" .. res.status)
+			log.info(res.response)
+		end
+		M.BUSY = false
+	end)
+end
+
+function M.check_version()
+	-- Call this through a flow
+	M.BUSY = true
+	if M.releases == nil then
+		M.get_latest()
+		flow.until_true(function() return not M.BUSY end)
+		if M.releases then
+			local current_version = M.current_version()
+			if M.releases.latest == current_version then
+				return true, M.releases.latest
+			else
+				return false, M.releases.latest
+			end
+		end
+	end
+end
+
+function M.current_version()
+	return sys.get_config("project.version")
+end
+
+return M
