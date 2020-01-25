d5d4c2b84653c25e0d291b2dcc16d170a27bc5bf
Jerakin <the_rensvind@hotmail.com>
2020-01-26 00:40:46 +0100

    Return second data sequential times

--- screens/popups/version_dialog/version.lua
+++ screens/popups/version_dialog/version.lua
@@ -1,4 +1,6 @@
 local flow = require "utils.flow"
+local log = require "utils.log"
+
 local M = {}
 
 local version_url = "https://raw.githubusercontent.com/Jerakin/Pokedex5E/master/assets/datafiles/releases.json"
@@ -27,12 +29,13 @@ end
 function M.check_version()
 	-- Call this through a flow
 	M.BUSY = true
+	local current_version = M.current_version()
 	if M.releases == nil then
 		M.get_latest()
 		flow.until_true(function() return not M.BUSY end)
 		if M.releases then
-			local current_version = M.current_version()
 			if M.releases[current_version] == nil then
+				log.info("Current version not in version json")
 				return
 			end
 			if M.releases.latest == M.releases[current_version].number then
@@ -41,6 +44,8 @@ function M.check_version()
 				return false, M.releases.latest - M.releases[current_version].number, M.releases[current_version].url
 			end
 		end
+	else
+		return M.releases.latest - M.releases[current_version].number==0, M.releases.latest - M.releases[current_version].number, M.releases[current_version].url
 	end
 end
 
