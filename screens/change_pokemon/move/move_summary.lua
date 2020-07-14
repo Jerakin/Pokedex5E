0e348fa89215b686733e2fcf7cc7c86e41b2091b
MagRoader <magroader@gmail.com>
2020-07-14 10:03:30 -0400

    Switching move summary screen to more OO style to make future scrollbars easier

--- screens/change_pokemon/move/move_summary.lua
+++ screens/change_pokemon/move/move_summary.lua
@@ -4,8 +4,6 @@ local _pokemon = require "pokedex.pokemon"
 local gui_utils = require "utils.gui"
 local gooey = require "gooey.gooey"
 
-local M = {}
-
 local function join_table(title, T, sep)
 	if T then
 		return title .. table.concat(T, sep)
@@ -13,15 +11,31 @@ local function join_table(title, T, sep)
 	return "-"
 end
 
-local function update_desc_list(str_node_prefix, action_id, action)
-	gooey.vertical_static_list(str_node_prefix, str_node_prefix .. "/desc_stencil", {str_node_prefix .. "/txt_desc"}, action_id, action, function() end)
+local function initial_setup(data)
 end
 
-function M.get_size(str_node_prefix)
-	return gui.get_size(gui.get_node(str_node_prefix .. "/background"))
+local function update_desc_list(data, action_id, action)
+	gooey.vertical_static_list(data.str_node_prefix, data.str_node_prefix .. "/desc_stencil", {data.str_node_prefix .. "/txt_desc"}, action_id, action, function() end)
 end
 
-function M.setup_move(str_node_prefix, pokemon, move_name)
+local MOVE_SUMMARY = {}
+
+function MOVE_SUMMARY.get_size(data)
+	return gui.get_size(gui.get_node(data.str_node_prefix .. "/background"))
+end
+
+function MOVE_SUMMARY.on_input(data, action_id, action)
+	update_desc_list(data, action_id, action)
+end
+
+function MOVE_SUMMARY.is_valid_move(data)
+	return data.is_valid_move
+end
+
+
+local M = {}
+
+function M.create(str_node_prefix, pokemon, move_name)
 
 	local move_data = nil
 	if move_name ~= nil then
@@ -31,11 +45,21 @@ function M.setup_move(str_node_prefix, pokemon, move_name)
 		-- nice to see what kind of bonus damage you'd get.
 		move_data = _pokemon.get_move_data(pokemon, move_name)
 	end
-	local is_valid = move_data ~= nil
+	local is_valid_move = move_data ~= nil
+
+
+	local data =
+	{
+		str_node_prefix = str_node_prefix,
+		pokemon = pokemon,
+		move_name = move_name,
+		is_valid_move = is_valid_move,
+	}
 	
-	gui.set_enabled(gui.get_node(str_node_prefix .. "/root"), is_valid)
 	
-	if is_valid then
+	gui.set_enabled(gui.get_node(str_node_prefix .. "/root"), is_valid_move)
+	
+	if is_valid_move then
 
 		-- NOTE: This code was in large part stolen from move_info.gui_script, which the move summary was based on. It has a similar general layout
 		-- but is more compressed to fit 2 on a screen better, and since it fits 2 on a screen we split it out into its own component
@@ -70,7 +94,7 @@ function M.setup_move(str_node_prefix, pokemon, move_name)
 		local diff_desc_size = metrics_desc_new.height - metrics_desc.height
 		size_desc.y = size_desc.y + diff_desc_size
 		gui.set_size(node_desc, size_desc)
-		update_desc_list(str_node_prefix)
+		update_desc_list(data)
 
 		gui_utils.scale_text_to_fit_size(node_name)
 		gui_utils.scale_text_to_fit_size(node_time)
@@ -91,11 +115,11 @@ function M.setup_move(str_node_prefix, pokemon, move_name)
 		end
 	end
 
-	return is_valid
-end
-
-function M.on_input(str_node_prefix, action_id, action)
-	update_desc_list(str_node_prefix, action_id, action)
+	local instance = {}
+	for name,fn in pairs(MOVE_SUMMARY) do
+		instance[name] = function(...) return fn(data, ...) end
+	end
+	return instance
 end
 
 return M
