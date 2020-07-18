5a5aeb31a5b8406bfcd3ad6a9a5a84d92099f9bd
Jerakin <the_rensvind@hotmail.com>
2020-07-18 18:22:52 +0200

    QR reading and sharing works

--- pokedex/share.lua
+++ pokedex/share.lua
@@ -6,20 +6,12 @@ local monarch = require "monarch.monarch"
 local dex = require "pokedex.dex"
 local pokedex = require "pokedex.pokedex"
 local statuses = require "pokedex.statuses"
+local _file = require "utils.file"
 
 local M = {}
 
-local function load_json(j)
-	local json_data = nil
-	-- Use pcall to catch possible parse errors so that we can print out the name of the file that we failed to parse
-	if pcall(function() json_data = json.decode(j) end) then
-		return json_data
-	else
-		return nil
-	end
-end
 
-local function validate(pokemon)
+function M.validate(pokemon)
 	if pokemon and type(pokemon) == "table" and pokemon.species and pokemon.species.current and
 	pokemon.hp and pokemon.hp.current then
 		return true
@@ -28,9 +20,9 @@ local function validate(pokemon)
 end
 
 function M.import()
-	local pokemon = load_json(clipboard.paste())
+	local pokemon = _file.load_json(clipboard.paste())
 	if pokemon then
-		if not validate(pokemon) then
+		if not M.validate(pokemon) then
 			notify.notify("Pokemon data is incomplete")
 			notify.notify(clipboard.paste())
 			return 
@@ -53,7 +45,7 @@ function M.import()
 		notify.notify(clipboard.paste())
 	end
 end
-local function encode_status(pokemon)
+function M.encode_status(pokemon)
 	local new = {}
 	for s, _ in pairs(pokemon.statuses or {}) do
 		new[statuses.string_to_state[s]] = true
@@ -62,46 +54,15 @@ local function encode_status(pokemon)
 end
 
 function M.get_clipboard()
-	local pokemon = load_json(clipboard.paste())
+	local pokemon = _file.load_json(clipboard.paste())
 	if pokemon then
-		if not validate(pokemon) then
+		if not M.validate(pokemon) then
 			return 
 		end
-		encode_status(pokemon)
+		M.encode_status(pokemon)
 		return pokemon
 	end
 	return
-end
-
-function M.load_qr()
-	if camera.start_capture(camera.CAMERA_TYPE_BACK, camera.CAPTURE_QUALITY_HIGH) then
---[[
-		-- start loop
-
-		-- camera buffer should be:
-		local info = camera.get_info()
-		-- info has width, height
-
-		-- has stream named rgb, type buffer,VALUE_TYPE_UINT8, value count 1
-		local buffer = camera.get_frame()
-
-		-- (Could theoretically display this if needed?)
-
-		-- I'm not clear on whether the buffer is in the right format for qrcode, qrcore says it needs "An image buffer where the first stream must be of format UINT8 * 3, and have the dimensions width*height"
-		-- but, assuming it does,
-		local qrstring = qrcode.scan(buffer, info.width, info.height, 0) -- 0 is flip_x
-
-		if qrstring ~= nil then
-			-- qr code found, can exit loop and check if it's a pokemon string
-			camera.stop_capture()
-		else
-			-- no qr code found
-		end
-
-		-- end loop
-
-		--]]
-	end
 end
 
 local function decode_status(pokemon)
