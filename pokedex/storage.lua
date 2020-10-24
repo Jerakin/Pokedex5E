73460d40f4ebd77e934530124496de67ecb834cf
MagRoader <magroader@gmail.com>
2020-10-24 17:46:17 -0400

    Fixed a typo I found while looking through the review

--- pokedex/storage.lua
+++ pokedex/storage.lua
@@ -50,7 +50,7 @@ end
 local function sort_on_index(a, b)
 	return function(a, b) 
 		local c = pokedex.get_index_number(_pokemon.get_current_species(a), _pokemon.get_variant(a))
-		local d = pokedex.get_index_number(_pokemon.get_current_species(b), _pokemon.get_variant(a))
+		local d = pokedex.get_index_number(_pokemon.get_current_species(b), _pokemon.get_variant(b))
 		return c < d  
 	end
 end
