daf550591a91e6d0b231b0a55e6bb165982244bf
MagRoader <magroader@gmail.com>
2020-10-24 17:46:27 -0400

    Variants waits to load file until it's needed

--- pokedex/variants.lua
+++ pokedex/variants.lua
@@ -3,7 +3,7 @@ local pokedex = require "pokedex.pokedex"
 
 local profiles = require "pokedex.profiles" -- HACK, SEE USAGE
 
-local variant_map = {}
+local variant_map = nil
 local initialized = false
 
 local M = {}
@@ -11,15 +11,6 @@ local M = {}
 function M.init()
 
 	if not initialized then
-		-- Load up the variant mapping file. This goes {species : [var1, var2]} 
-		-- Here we change it to {var1: {species: species}, var2: {species: species}}
-		local var_map_file = file.load_json_from_resource("/assets/datafiles/variant_map.json")
-		for s,v in pairs(var_map_file) do
-			for i=1, #v do
-				variant_map[v[i]] = {species=s}
-			end
-		end
-
 		-- SUPER HACK - profiles is tied into, like, everything. But also it stores the profile's current party, which
 		-- needs to be upgraded to use the species/variant system. But since so many things depend on profiles, I can't
 		-- tell profiles to depend on variants, because that would be a circular reference.
@@ -32,6 +23,19 @@ end
 
 
 function M.get_species_variant_for(original_name)
+
+	if not variant_map then
+		variant_map = {}
+		-- Load up the variant mapping file. This goes {species : [var1, var2]} 
+		-- Here we change it to {var1: {species: species}, var2: {species: species}}
+		local var_map_file = file.load_json_from_resource("/assets/datafiles/variant_map.json")
+		for s,v in pairs(var_map_file) do
+			for i=1, #v do
+				variant_map[v[i]] = {species=s}
+			end
+		end
+	end
+
 	local obj = variant_map[original_name]
 	if obj then
 		if not obj.variant then
